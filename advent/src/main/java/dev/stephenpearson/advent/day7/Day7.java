cb82c691884e3127c70877a1271fb70b8c58f885
StephenPearsonDev <pearsonsw@gmail.com>
2024-12-11 08:27:13 +0100

    day 7

--- advent/src/main/java/dev/stephenpearson/advent/day7/Day7.java
+++ advent/src/main/java/dev/stephenpearson/advent/day7/Day7.java
@@ -39,7 +39,6 @@ public class Day7 {
                     operands.add(Arrays.stream(parts[1].trim().split("\\s+"))
                             .mapToInt(Integer::parseInt)
                             .toArray());
-
                 }
 
             }
@@ -48,7 +47,7 @@ public class Day7 {
             e.printStackTrace();
         }
 
-        // Total sum of all valid target sums - took me ages to figure I was just counting the number of possible true equations!
+        //Total sum of all valid target sums - took me ages to figure I was just counting the number of possible true equations!
         long calibrationResult = 0;
 
         for (int i = 0; i < sums.size(); i++) {
@@ -62,12 +61,79 @@ public class Day7 {
 
        
         }
+        
+        System.out.println("part 1: " + calibrationResult);
+        
+        
+        long calibrationResultPart2 = 0;
+
+        for (int i = 0; i < sums.size(); i++) {
+            Long targetSum = sums.get(i);
+            int[] currentOperands = operands.get(i);
+
+            if (findNewTargetSum(targetSum, currentOperands)) {
+            	calibrationResultPart2 += targetSum;
+            }
+
+       
+        }
+        
+        System.out.println("part 1: " + calibrationResultPart2);
+        		
 
 
-//        System.out.println(sums.size());
+        
+        
 
-        // Print the total calibration result
-        System.out.println("Possible true sums: " + calibrationResult);
+        
+        //whats an easy way to pass in a new version of the operands with a sliding window of two operands fused?
+        
+//        long newCalibrationResult = 0;
+//
+//        for (int i = 0; i < sums.size(); i++) {
+//            Long targetSum = sums.get(i);
+//            int[] currentOperands = operands.get(i);
+//
+//            
+//            //this is a poor approach - but its the only way i can start
+//            for(int c = 0; c < operands.size(); c++) {
+//            	
+//            	int[] operandsWithConcat = new int[currentOperands.length-1];
+//            	
+//            	
+//            	
+//            	int concatAIndex = c;
+//            	int concatBIndex = c+1;
+//            	
+//            	if(concatBIndex < operandsWithConcat.length) {
+//            		
+//            		long fused = concatinate(currentOperands[concatAIndex], currentOperands[concatBIndex]);
+//            		
+//            	}
+//            	
+//            	for(int t = 0; t < operandsWithConcat.length; i++) {
+//            		
+//            		
+//            		if(t != concatAIndex && t != concatBIndex) {
+//            			operandsWithConcat[t] = currentOperands[t];
+//            		} else  {
+//            			
+//            		}
+//            		
+//            		
+//            	}
+//            	
+//            	
+//            	if (findNewTargetSum(targetSum, currentOperands)) {
+//                    newCalibrationResult += targetSum;
+//                }
+//            }
+//            
+//            
+//
+//       
+//        }
+
     }
 
     // Exit early - if current sum becomes larger than target sum, move on
@@ -84,13 +150,23 @@ public class Day7 {
 
         return calculateRecursively(operands, 0, 0, targetSum);
     }
+    
+    private static boolean findNewTargetSum(Long targetSum, int[] operands) {
+
+    	   
+        if (operands.length == 1) {
+            return operands[0] == targetSum;
+        }
+        
+
+        return calculateNewCalibrationResultRecursively(operands, 0, operands[0], targetSum);
+    }
+    
+    
+    
 
     private static boolean calculateRecursively(int[] operands, int index, long currentSum, long targetSum) {
 
-//
-//        System.out.println("Index: " + index + ", CurrentSum: " + currentSum + ", TargetSum: " + targetSum);
-
- 
         if (currentSum > targetSum) {
             return false;
         }
@@ -113,7 +189,48 @@ public class Day7 {
             }
         }
 
-
         return false;
+    }
+    
+    
+    //new operand || merges the numbers. First thought was just use a kind of sliding concat window to jump through the operands like before.
+    public static boolean calculateNewCalibrationResultRecursively(int[] operands, int index, long currentSum, long targetSum) {
+    	
+    	if (currentSum > targetSum) {
+            return false;
+        }
+
+    	 if (index == operands.length - 1) {
+             return currentSum == targetSum;
+         }
+        
+        int nextOperand = operands[index + 1];
+        
+        //add
+        if (calculateNewCalibrationResultRecursively(operands, index + 1,  currentSum + nextOperand, targetSum)) {
+            return true;
+        }
+
+       //multiply
+        if (nextOperand > 0 && currentSum <= targetSum / nextOperand) {
+            if (calculateNewCalibrationResultRecursively(operands, index + 1, currentSum * nextOperand, targetSum)) {
+                return true;
+            }
+        }
+        
+        long concatenatedValue = concatinate(currentSum, nextOperand);
+        if (calculateNewCalibrationResultRecursively(operands, index + 1, concatenatedValue, targetSum)) {
+            return true;
+        }
+
+    	
+    	return false;
+    }
+    
+    
+    
+    //cant remember how to do it mathematically, I remember involves multiplication using 10
+    public static long concatinate(long a, long b) {
+    	return Long.parseLong(String.valueOf(a) + String.valueOf(b));
     }
 }
